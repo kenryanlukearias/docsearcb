c3562f6d57071b64f034d6ec1c6113baa2e515db
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-05-30 17:48:01 +0400

    initial push

--- frontend/docdirect/src/app/home/landing/landing-page.component.ts
+++ frontend/docdirect/src/app/home/landing/landing-page.component.ts
@@ -4,10 +4,11 @@ import { AboutPageComponent } from "../about-us/about-page.component";
 import { NavbarComponent } from "../../shared/nav/navbar.component";
 import { FooterPageComponent } from "../../shared/footer/footer-page.component";
 import { BookingPageComponent } from "../book/booking-page.component";
+import { PlatformComponent } from "../platform/platform.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HomePageComponent, AboutPageComponent, NavbarComponent, FooterPageComponent, BookingPageComponent],
+  imports: [HomePageComponent, AboutPageComponent, NavbarComponent, FooterPageComponent, BookingPageComponent, PlatformComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
---
+++ frontend/docdirect/src/app/home/platform/platform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-platform',
+  imports: [],
+  templateUrl: './platform.component.html',
+  styleUrl: './platform.component.scss'
+})
+export class PlatformComponent {
+
+}
