b77b08a70213f256d36b9139067a067ba0b2c009
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-05-31 19:51:31 +0400

    COMPLETE FINISH

--- frontend/docdirect/src/app/shared/drawer/drawer.component.ts
+++ frontend/docdirect/src/app/shared/drawer/drawer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-drawer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './drawer.component.html',
   styleUrl: './drawer.component.scss'
 })
